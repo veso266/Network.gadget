b947f852ba980f32e331a4abe6ba8bf6f55a4033
veso266 <veso266@gmail.com>
2020-01-19 11:58:27 +0100

    Added api files

---
+++ api/gadget/vista/NetworkService.php
@@ -0,0 +1,108 @@
+<?php
+
+//Some Helper Functions to get IP adress from some API
+function ip_details($isp) {
+    $json = file_get_contents("http://ipinfo.io/{$isp}");
+    $details = json_decode($json);
+    return $details;
+}
+function get_ip_address(){
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+        if (array_key_exists($key, $_SERVER) === true){
+            foreach (explode(',', $_SERVER[$key]) as $ip){
+                $ip = trim($ip); // just to be safe
+
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                    return $ip;
+                }
+            }
+        }
+    }
+}
+
+function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
+    $output = NULL;
+    if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+        $ip = $_SERVER["REMOTE_ADDR"];
+        if ($deep_detect) {
+            if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+    }
+    $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+    $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+    $continents = array(
+        "AF" => "Africa",
+        "AN" => "Antarctica",
+        "AS" => "Asia",
+        "EU" => "Europe",
+        "OC" => "Australia (Oceania)",
+        "NA" => "North America",
+        "SA" => "South America"
+    );
+    if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+        $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+        if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+            switch ($purpose) {
+                case "location":
+                    $output = array(
+                        "city"           => @$ipdat->geoplugin_city,
+                        "state"          => @$ipdat->geoplugin_regionName,
+                        "country"        => @$ipdat->geoplugin_countryName,
+                        "country_code"   => @$ipdat->geoplugin_countryCode,
+                        "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                        "continent_code" => @$ipdat->geoplugin_continentCode
+                    );
+                    break;
+                case "address":
+                    $address = array($ipdat->geoplugin_countryName);
+                    if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                        $address[] = $ipdat->geoplugin_regionName;
+                    if (@strlen($ipdat->geoplugin_city) >= 1)
+                        $address[] = $ipdat->geoplugin_city;
+                    $output = implode(", ", array_reverse($address));
+                    break;
+                case "city":
+                    $output = @$ipdat->geoplugin_city;
+                    break;
+                case "state":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "region":
+                    $output = @$ipdat->geoplugin_regionName;
+                    break;
+                case "country":
+                    $output = @$ipdat->geoplugin_countryName;
+                    break;
+                case "countrycode":
+                    $output = @$ipdat->geoplugin_countryCode;
+                    break;
+            }
+        }
+    }
+    return $output;
+}
+//Some Helper Functions
+
+
+
+
+$ip = $ip = $_SERVER['REMOTE_ADDR'];
+$details = ip_details("$ip");
+$ISP = substr($details->org, 7);
+
+
+$ip_user = $_SERVER['REMOTE_ADDR'];
+$Country = ip_info("$ip", "Country");
+$ISPDB = $details->org;
+$UserAgent = $_SERVER['HTTP_USER_AGENT'];
+
+?>
+<!--- ORIGINAL <p style="margin: 10px auto 0 auto; margin-right: 6.80315px; font-size: 15px; text-align: right;"><?php echo get_ip_address()?></p><!--</p>--->
+<!--<div style="background-color:#ccc;"> --->
+<p style="margin: 10px auto 0 auto; margin-right: 0; font-size: 15px; text-align: center;"><?php echo get_ip_address()?></p><!--</p>--->
+<p style="cursor: pointer; padding: 0; margin: 0; text-align: center;" onclick="tryAgain(); return false;"><b>ISP: <?php echo $ISP ?></b></p>
+</div>
+<!--<p style="cursor: pointer; padding: 0; margin: 0; text-align: center;" onclick="tryAgain(); return false;"><b>- Refresh -</b></p> --->
